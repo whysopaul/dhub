3c045dca8766d787f77d5c5c5896f3929219309c
whysopaul <whysopaul@gmail.com>
2023-05-25 21:04:49 +0200

    breadcrumbs update

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -7,6 +7,8 @@ import { useSelector } from 'react-redux';
 import { RootStore } from '../../store';
 import UserHeader from './UserHeader';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { mockArtData } from '../../actions/articles/articles';
+import { createServiceLink } from '../utils';
 
 interface IHeaderProps {
     root?: boolean
@@ -19,20 +21,70 @@ const Header: React.FunctionComponent<IHeaderProps> = (props) => {
     const navigate = useNavigate()
     const location = useLocation()
 
-    const createBreadCrumbs = (i: string) => {
-        switch (i) {
-            case '':
-                return <li><Link to='/'>Главная</Link></li>
-            case 'service':
-                return <li><Link to='/services'>Сервисы</Link></li>
-            case 'services':
-                return <li><span>Сервисы</span></li>
-            case 'profile':
-                return <li><Link to='/profile'>Профиль</Link></li>
-            case 'edit':
-                return <li><span>Редактировать профиль</span></li>
-            default:
-                return <li><span>{i}</span></li>
+    const createBreadCrumbs = (path: string) => {
+
+        let pathArray = path.split('/')
+        pathArray[pathArray.length - 1] === '' && pathArray.pop()
+
+        if (path.includes('service')) {
+            return pathArray.map(link => {
+                switch (link) {
+                    case '':
+                        return <li><Link to='/'>Главная</Link></li>
+                    case 'service':
+                        return <li><Link to='/services'>Сервисы</Link></li>
+                    case 'services':
+                        return <li><span>Сервисы</span></li>
+                    default:
+                        return <li><span>{rootState.services.services.find(service => createServiceLink(service.name) === decodeURI(link))?.name}</span></li>
+                }
+            })
+        }
+
+        if (path.includes('feedback')) {
+            return pathArray.map(link => {
+                switch (link) {
+                    case '':
+                        return <li><Link to='/'>Главная</Link></li>
+                    case 'feedback':
+                        return <li><span>Отзывы</span></li>
+                    default:
+                        return null
+                }
+            })
+        }
+
+        if (path.includes('article')) {
+            return pathArray.map(link => {
+                switch (link) {
+                    case '':
+                        return <li><Link to='/'>Главная</Link></li>
+                    case 'article':
+                        return <li><Link to='/articles'>Статьи</Link></li>
+                    case 'articles':
+                        return <li><span>Статьи</span></li>
+                    default:
+                        return <li><span>{mockArtData.find(article => article.id === parseInt(link)).title}</span></li>
+                }
+            })
+        }
+
+        if (path.includes('profile')) {
+            return pathArray.map((link, idx) => {
+                switch (link) {
+                    case '':
+                        return <li><Link to='/'>Главная</Link></li>
+                    case 'profile':
+                        if (idx + 1 === pathArray.length) {
+                            return <li><span>Профиль</span></li>
+                        }
+                        return <li><Link to='/profile'>Профиль</Link></li>
+                    case 'edit':
+                        return <li><span>Редактировать профиль</span></li>
+                    default:
+                        return null
+                }
+            })
         }
     }
 
@@ -49,7 +101,7 @@ const Header: React.FunctionComponent<IHeaderProps> = (props) => {
             {!props.root && <>
                 <div className={styles.breadcrumbs}>
                     <ul>
-                        {location.pathname.split('/').map(i => createBreadCrumbs(i))}
+                        {createBreadCrumbs(location.pathname)}
                     </ul>
                 </div>
             </>}
